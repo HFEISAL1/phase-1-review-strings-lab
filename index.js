72a15bb9693a2146987e6f9b8d9d6eba32af298c
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-06-07 12:49:24 -0700

    stringlab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+let currentUser = 'halima' ;
+
+let welcomemessage 'Welcome to flatbook, ';
+welcomemessage = 'welcome to flatbook,  +${currentUser}'!
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK,  ${currentUser.topUpperCase()} !';
+
+let ShortGreeting = 'Welcome, ${currentUser.slice(0, 1)}!'
